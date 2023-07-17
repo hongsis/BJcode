1609493139d7eb507ab8f7cea9d25fa01978faf3
hongsis <121384731+hongsis@users.noreply.github.com>
2023-07-17 20:54:55 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,11 @@
+N, M=map(int,input().split())
+bsk=list(range(1,N+1))
+temp=0
+for _ in range(M):
+    i, j=map(int,input().split())
+    temp=bsk[i-1]
+    bsk[i-1]=bsk[j-1]
+    bsk[j-1]=temp
+    
+for n in range(N):
+    print(bsk[n],end=' ')
