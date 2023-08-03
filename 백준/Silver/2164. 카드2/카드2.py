7db844b43e46bab8767365fa35afbeb7a324b8e9
hongsis <121384731+hongsis@users.noreply.github.com>
2023-08-03 17:33:34 +0900

    [Silver IV] Title: 카드2, Time: 264 ms, Memory: 55104 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque
+n=int(input())
+q=deque([i for i in range(1,n+1)])
+
+while (len(q)>1):
+        q.popleft()
+        a=q.popleft()
+        q.append(a)
+print(q[0])
+
+    
+
+
+
