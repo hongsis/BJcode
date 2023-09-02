2db9d889d5dd65bc8c12fade3677a41b6b453b68
hongsis <121384731+hongsis@users.noreply.github.com>
2023-09-02 23:15:23 +0900

    [Bronze V] Title: 학점계산, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,33 @@
+s=input()
+
+if s=='A+':
+    print(4.3)
+elif s=='A0':
+    print(4.0)
+elif s=='A-':
+    print(3.7)    
+elif s=='B+':
+    print(3.3)
+elif s=='B0':
+    print(3.0)
+elif s=='B-':
+    print(2.7)    
+elif s=='C+':
+    print(2.3)
+elif s=='C0':
+    print(2.0)
+elif s=='C-':
+    print(1.7)   
+elif s=='D+':
+    print(1.3)
+elif s=='D0':
+    print(1.0)
+elif s=='D-':
+    print(0.7)     
+else:
+    print(0.0)    
+    
+    
+    
+    
+    
