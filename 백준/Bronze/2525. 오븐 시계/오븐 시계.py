2213332afa8acb7ec01ea747f5062488d6cd886d
hongsis <121384731+hongsis@users.noreply.github.com>
2024-09-17 17:32:49 +0900

    [Bronze III] Title: 오븐 시계, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,24 +1,14 @@
-current_time = input().split()
-hour = int(current_time[0])
-minute = int(current_time[1])
+A, B=map(int,input().split())
+C=int(input())
 
-# 요리 시간 입력 받기
-cooking_time = int(input())
 
-# 종료되는 시각 계산
-hour += cooking_time // 60
-minute += cooking_time % 60
+B += C
 
-# 시간과 분이 60 이상인 경우 조정
-if minute >= 60:
-    hour += 1
-    minute -= 60
-if hour >= 24:
-    hour -= 24
+A += int(B/60)
 
-# 종료되는 시각 출력
-print(hour, minute)
+B= B %60
 
+if A>=24:
+    A-=24
     
-    
-    
+print(A,B)
